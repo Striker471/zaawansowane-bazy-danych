35219b18f4c8adf3fc01d0273f4f566a5529ca26
Jakub Budzilo <84582825+Striker471@users.noreply.github.com>
2024-04-21 19:14:00 +0200

    test fix

--- src/test/java/com/example/zbd/ZbdApplicationTests.java
+++ src/test/java/com/example/zbd/ZbdApplicationTests.java
@@ -1,25 +1,14 @@
 package com.example.zbd;
 
 import com.example.zbd.repository.BookRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ZbdApplicationTests {
 
     @Autowired
     private BookRepository bookRepository;
-
-
-    @Test
-    void contextLoads() {
-        var x = bookRepository.findByGenreName("elo");
-        assertEquals("elo", x);
-    }
-
-
 }
